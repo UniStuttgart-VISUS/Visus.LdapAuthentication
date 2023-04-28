21a34e8f7b614dc775081e020cc2e0818edf6b7d
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-04-28 16:22:21 +0200

    Added additional LdapUserExtensions that enable the caller to set the authenticationType or the identity/principal created.

--- Visus.LdapAuthentication/LdapUserExtensions.cs
+++ Visus.LdapAuthentication/LdapUserExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="LdapUserExtensions.cs" company="Visualisierungsinstitut der Universität Stuttgart">
-// Copyright © 2021 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// Copyright © 2021 - 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
 // </copyright>
 // <author>Christoph Müller</author>
 
@@ -14,6 +14,41 @@ namespace Visus.LdapAuthentication {
     /// Extension methods for <see cref="ILdapUser"/> and derived classes.
     /// </summary>
     public static class LdapUserExtensions {
+
+        /// <summary>
+        /// Creates a <see cref="ClaimsIdentity"/> for the given LDAP user
+        /// limiting the claims to the ones that pass <paramref name="filter"/>.
+        /// </summary>
+        /// <param name="that">The LDAP user to create the identity for. It is
+        /// safe to pass <c>null</c>, in which case the return will be
+        /// <c>null</c> as well.</param>
+        /// <param name="authenticationType">The name of the authentication
+        /// type used to create the <see cref="ClaimsIdentity"/>.</param>
+        /// <param name="filter">A function that checks each claim before it
+        /// is being added. It is safe to pass <c>null</c>, in which case
+        /// all claims from <see cref="ILdapUser.Claims"/> will be added. This
+        /// parameter defaults to <c>null</c>.
+        /// </param>
+        /// <returns>The claims identity for the given user.</returns>
+        public static ClaimsIdentity ToClaimsIdentity(this ILdapUser that,
+                string authenticationType,
+                Func<Claim, bool> filter = null) {
+            if (that == null) {
+                // There is no user, so the identity is null, too.
+                return null;
+            }
+
+            if (filter == null) {
+                // There is no filter, so return all claims.
+                return new ClaimsIdentity(that.Claims, authenticationType);
+            }
+
+            // Return filtered claims.
+            var claims = from c in that.Claims
+                         where filter(c)
+                         select c;
+            return new ClaimsIdentity(claims, authenticationType);
+        }
 
         /// <summary>
         /// Creates a <see cref="ClaimsIdentity"/> for the given LDAP user
@@ -34,23 +69,32 @@ namespace Visus.LdapAuthentication {
         /// <returns>The claims identity for the given user.</returns>
         public static ClaimsIdentity ToClaimsIdentity(this ILdapUser that,
                 Func<Claim, bool> filter = null) {
-            if (that == null) {
-                // There is no user, so the identity is null, too.
+            return that.ToClaimsIdentity(nameof(ILdapAuthenticationService),
+                filter);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="ClaimsPrincipal"/> for the given LDAP user.
+        /// </summary>
+        /// <param name="that">The LDAP user to create the principal for. It is
+        /// safe to pass <c>null</c>, in which case the return will be
+        /// <c>null</c> as well.</param>
+        /// <param name="authenticationType">The name of the authentication
+        /// type used to create the <see cref="ClaimsIdentity"/>.</param>
+        /// <param name="filter">A function that checks each claim before it
+        /// is being added. It is safe to pass <c>null</c>, in which case
+        /// all claims from <see cref="ILdapUser.Claims"/> will be added. This
+        /// parameter defaults to <c>null</c>.
+        /// </param>
+        /// <returns>The principal for the given user.</returns>
+        public static ClaimsPrincipal ToClaimsPrincipal(this ILdapUser that,
+                string authenticationType, Func<Claim, bool> filter = null) {
+            if (that != null) {
+                return new ClaimsPrincipal(that.ToClaimsIdentity(
+                    authenticationType, filter));
+            } else {
                 return null;
             }
-
-            var authType = nameof(ILdapAuthenticationService);
-
-            if (filter == null) {
-                // There is no filter, so return all claims.
-                return new ClaimsIdentity(that.Claims, authType);
-            }
-
-            // Return filtered claims.
-            var claims = from c in that.Claims
-                         where filter(c)
-                         select c;
-            return new ClaimsIdentity(claims, authType);
         }
 
         /// <summary>
