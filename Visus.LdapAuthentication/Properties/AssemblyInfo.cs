68780ca094cb54e2272c05f6e69b8c66ee30212b
Christoph Müller <christoph.mueller@hotmail.de>
2023-12-16 18:33:22 +0100

    Enable testing of internal classes.

---
+++ Visus.LdapAuthentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Visus.LdapAuthentication.Tests")]
