0bdaf48fda47bd596c25b8fce23f3f918a23bea9
Christoph Müller <christoph.mueller@hotmail.de>
2024-01-21 12:46:06 +0100

    Fix Moq dependency. Added DirectoryIdentity project.

---
+++ Visus.DirectoryIdentity/ILdapIdentityUser.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ILdapIdentityUser.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+using Visus.DirectoryAuthentication;
+
+
+namespace Visus.DirectoryIdentity {
+
+    /// <summary>
+    /// The interface for an LDAP user object to be used with the identity
+    /// store.
+    /// </summary>
+    public interface ILdapIdentityUser : ILdapUser { }
+}
