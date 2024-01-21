1ba48570acdd06ce50c8d030f2452a43fefab8b5
Christoph Müller <christoph.mueller@hotmail.de>
2024-01-21 17:41:08 +0100

    Add extension method for injection ILdapOptions.

--- Visus.DirectoryAuthentication/ServiceCollectionExtensions.cs
+++ Visus.DirectoryAuthentication/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // <author>Christoph Müller</author>
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -37,12 +38,57 @@ namespace Visus.DirectoryAuthentication {
             _ = that ?? throw new ArgumentNullException(nameof(that));
             _ = options ?? throw new ArgumentNullException(nameof(options));
 
+            //that.AddSingleton(options);
+
             return that.AddScoped<ILdapAuthenticationService<TUser>,
                     LdapAuthenticationService<TUser>>(s => {
                 var l = s.GetService<ILogger<
                     LdapAuthenticationService<TUser>>>();
                 return new LdapAuthenticationService<TUser>(options, l);
             });
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ILdapAuthenticationService"/> to the dependency
+        /// injection container.
+        /// </summary>
+        /// <remarks>
+        /// This variant only works if the <see cref="ILdapOptions"/> have been
+        /// registered in the container as well, for instance using
+        /// <see cref="AddLdapOptions"/>.
+        /// </remarks>
+        /// <typeparam name="TUser">The type of the user that is being
+        /// authenticated.</typeparam>
+        /// <param name="that">The service collection to add the service to.
+        /// </param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapAuthenticationService<TUser>(
+                this IServiceCollection that)
+                where TUser : class, ILdapUser, new() {
+            _ = that ?? throw new ArgumentNullException(nameof(that));
+            return that.AddScoped<ILdapAuthenticationService<TUser>,
+                LdapAuthenticationService<TUser>>();
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ILdapAuthenticationService"/> to the dependency
+        /// injection container.
+        /// </summary>
+        /// <remarks>
+        /// This variant only works if the <see cref="ILdapOptions"/> have been
+        /// registered in the container as well, for instance using
+        /// <see cref="AddLdapOptions"/>.
+        /// </remarks>
+        /// <param name="that">The service collection to add the service to.
+        /// </param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapAuthenticationService(
+                this IServiceCollection that) {
+            return that.AddLdapAuthenticationService<LdapUser>();
         }
 
         /// <summary>
@@ -89,6 +135,125 @@ namespace Visus.DirectoryAuthentication {
         }
 
         /// <summary>
+        /// Adds an <see cref="ILdapConnectionService"/> to the dependency
+        /// injection container.
+        /// </summary>
+        /// <remarks>
+        /// This variant only works if the <see cref="ILdapOptions"/> have been
+        /// registered in the container as well, for instance using
+        /// <see cref="AddLdapOptions"/>.
+        /// </remarks>
+        /// <param name="that">The service collection to add the service to.
+        /// </param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapConnectionService(
+                this IServiceCollection that) {
+            _ = that ?? throw new ArgumentNullException(nameof(that));
+            return that.AddScoped<ILdapConnectionService,
+                LdapConnectionService>();
+        }
+
+        /// <summary>
+        /// Adds the given <see cref="IConfigurationSection"/> as
+        /// <see cref="ILdapOptions"/>.
+        /// </summary>
+        /// <typeparam name="TOptions">The actual type of the configuration that
+        /// is to be instantiated.</typeparam>
+        /// <param name="that">The service collection to add the options to.
+        /// </param>
+        /// <param name="section">The configuration section mapped to the LDAP
+        /// options.</param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">If
+        /// <paramref name="section"/> is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapOptions<TOptions>(
+                this IServiceCollection that, IConfigurationSection section)
+                where TOptions : class, ILdapOptions {
+            _ = that ?? throw new ArgumentNullException(nameof(that));
+            _ = section ?? throw new ArgumentNullException(nameof(section));
+            return that.Configure<ILdapOptions>(section)
+                .Configure<TOptions>(section);
+        }
+
+        /// <summary>
+        /// Adds the given <see cref="IConfigurationSection"/> as
+        /// <see cref="LdapOptions"/>.
+        /// </summary>
+        /// <param name="section">The configuration section mapped to the LDAP
+        /// options.</param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">If
+        /// <paramref name="section"/> is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapOptions(
+                this IServiceCollection that, IConfigurationSection section) {
+            _ = that ?? throw new ArgumentNullException(nameof(that));
+            _ = section ?? throw new ArgumentNullException(nameof(section));
+            return that.Configure<ILdapOptions>(section)
+                .Configure<LdapOptions>(section);
+        }
+
+        /// <summary>
+        /// Adds the given specified <paramref name="section"/> of the
+        /// <see cref="IConfiguration"/> as <see cref="ILdapOptions"/>.
+        /// </summary>
+        /// <typeparam name="TOptions">The actual type of the configuration that
+        /// is to be instantiated.</typeparam>
+        /// <param name="that">The service collection to add the options to.
+        /// </param>
+        /// <param name="configuration">The configuration holding the LDAP
+        /// section.</param>
+        /// <param name="section">The name of the section holding the LDAP
+        /// options. This parameter defaults to &quot;LdapOptions&quot;.</param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">If
+        /// <paramref name="configuration"/> is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapOptions<TOptions>(
+                this IServiceCollection that,
+                IConfiguration configuration,
+                string section = "LdapOptions")
+                where TOptions : class, ILdapOptions {
+            _ = that
+                ?? throw new ArgumentNullException(nameof(that));
+            _ = configuration
+                ?? throw new ArgumentNullException(nameof(configuration));
+            var s = configuration.GetSection(section ?? "LdapOptions");
+            return that.AddLdapOptions<TOptions>(s);
+        }
+
+        /// <summary>
+        /// Adds the given specified <paramref name="section"/> of the
+        /// <see cref="IConfiguration"/> as <see cref="LdapOptions"/>.
+        /// </summary>
+        /// <param name="configuration">The configuration holding the LDAP
+        /// section.</param>
+        /// <param name="section">The name of the section holding the LDAP
+        /// options. This parameter defaults to &quot;LdapOptions&quot;.</param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">If
+        /// <paramref name="configuration"/> is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapOptions(
+                this IServiceCollection that,
+                IConfiguration configuration,
+                string section = "LdapOptions") {
+            _ = that
+                ?? throw new ArgumentNullException(nameof(that));
+            _ = configuration
+                ?? throw new ArgumentNullException(nameof(configuration));
+            var s = configuration.GetSection(section ?? "LdapOptions");
+            return that.AddLdapOptions<LdapOptions>(s);
+        }
+
+        /// <summary>
         /// Adds an <see cref="ILdapSearchService"/> to the dependency injection
         /// container.
         /// </summary>
@@ -116,7 +281,33 @@ namespace Visus.DirectoryAuthentication {
                 s => {
                     var l = s.GetService<ILogger<LdapSearchService<TUser>>>();
                     return new LdapSearchService<TUser>(options, l);
-            });
+                });
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ILdapSearchService"/> to the dependency injection
+        /// container.
+        /// </summary>
+        /// <remarks>
+        /// This variant only works if the <see cref="ILdapOptions"/> have been
+        /// registered in the container as well, for instance using
+        /// <see cref="AddLdapOptions"/>.
+        /// </remarks>
+        /// <typeparam name="TUser">The type of user to be created for the search
+        /// results, which also defines attributes like the unique identity in
+        /// combination with the global options from <see cref="ILdapOptions"/>.
+        /// </typeparam>
+        /// <param name="that">The service collection to add the service to.
+        /// </param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapSearchService<TUser>(
+                this IServiceCollection that)
+                where TUser : class, ILdapUser, new() {
+            _ = that ?? throw new ArgumentNullException(nameof(that));
+            return that.AddScoped<ILdapSearchService<TUser>,
+                LdapSearchService<TUser>>();
         }
 
         /// <summary>
@@ -135,6 +326,25 @@ namespace Visus.DirectoryAuthentication {
         public static IServiceCollection AddLdapSearchService(
                 this IServiceCollection that, ILdapOptions options) {
             return that.AddLdapSearchService<LdapUser>(options);
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ILdapSearchService"/> to the dependency injection
+        /// container.
+        /// </summary>
+        /// <remarks>
+        /// This variant only works if the <see cref="ILdapOptions"/> have been
+        /// registered in the container as well, for instance using
+        /// <see cref="AddLdapOptions"/>.
+        /// </remarks>
+        /// <param name="that">The service collection to add the service to.
+        /// </param>
+        /// <returns><paramref name="that"/> after injection.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="that"/>
+        /// is <c>null</c>.</exception>
+        public static IServiceCollection AddLdapSearchService(
+                this IServiceCollection that) {
+            return that.AddLdapSearchService<LdapUser>();
         }
     }
 }
