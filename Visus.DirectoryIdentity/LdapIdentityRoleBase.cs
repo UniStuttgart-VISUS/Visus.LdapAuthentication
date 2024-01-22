b0ff7fa54de3e5e9010103c12f2fd20e3cfb4264
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-01-22 17:07:59 +0100

    Added trivial implementations in LdapRoleStore.

---
+++ Visus.DirectoryIdentity/LdapIdentityRoleBase.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="LdapIdentityRoleBase.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Visus.DirectoryAuthentication;
+
+
+namespace Visus.DirectoryIdentity {
+
+    /// <summary>
+    /// The base class for a role object, which provides the default mappings
+    /// for ADDS.
+    /// </summary>
+    /// <remarks>
+    /// This class can be used as base class for custom roles that need
+    /// additional properties from the directory. Using this annotated base
+    /// makes shure that the store can find the necessary LDAP properties.
+    /// It is, however, possible to create a all-new class and annotating
+    /// it similarly with <see cref="LdapAttributeAttribute"/>s.
+    /// </remarks>
+    public abstract class LdapIdentityRoleBase {
+
+        /// <summary>
+        /// Gets the claims of the role.
+        /// </summary>
+        public virtual IEnumerable<Claim> Claims { get; internal set; }
+            = Enumerable.Empty<Claim>();
+
+        /// <summary>
+        /// Gets the name of the group that represents the role.
+        /// </summary>
+        [LdapAttribute(Schema.ActiveDirectory, "sAMAccountName")]
+        [LdapAttribute(Schema.IdentityManagementForUnix, "sAMAccountName")]
+        [LdapAttribute(Schema.Rfc2307, "gid")]
+        [Claim(ClaimTypes.Name)]
+        [Claim(ClaimTypes.WindowsAccountName)]
+        public virtual string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the unique identitfier of the role, which is equivalent to the
+        /// unqiue ID of the group in this implementation.
+        /// </summary>
+        [LdapAttribute(Schema.ActiveDirectory, "objectSid",
+            Converter = typeof(SidConverter))]
+        [LdapAttribute(Schema.IdentityManagementForUnix, "gidNumber")]
+        [LdapAttribute(Schema.Rfc2307, "gidNumber")]
+        [Claim(ClaimTypes.PrimarySid)]
+        [Claim(ClaimTypes.Sid)]
+        [Claim(ClaimTypes.NameIdentifier)]
+        public virtual string ID { get; internal set; }
+    }
+}
--- Visus.DirectoryIdentity/LdapRoleStore.cs
+++ Visus.DirectoryIdentity/LdapRoleStore.cs
@@ -9,72 +9,179 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Visus.DirectoryAuthentication;
 
 
 namespace Visus.DirectoryIdentity {
 
-    internal class LdapRoleStore<TRole> : IQueryableRoleStore<TRole>,
+    /// <summary>
+    /// A role store, which is based on security groups on the LDAP server.
+    /// </summary>
+    /// <typeparam name="TRole">The object used to represent a role (group).
+    /// </typeparam>
+    public class LdapRoleStore<TRole> : IQueryableRoleStore<TRole>,
             IRoleClaimStore<TRole>
             where TRole : class {
+
+        /// <summary>
+        /// Gets all groups in the directory.
+        /// </summary>
         public IQueryable<TRole> Roles => throw new NotImplementedException();
 
-        public Task AddClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="claim"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task AddClaimAsync(TRole role,
+                Claim claim,
+                CancellationToken cancellationToken = default) {
             throw new NotImplementedException();
         }
 
-        public Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task<IdentityResult> CreateAsync(TRole role,
+                CancellationToken cancellationToken) {
             throw new NotImplementedException();
         }
 
-        public Task<IdentityResult> DeleteAsync(TRole role, CancellationToken cancellationToken) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task<IdentityResult> DeleteAsync(TRole role,
+                CancellationToken cancellationToken) {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public void Dispose() {
+            if (this._searchService != null) {
+                this._searchService.Dispose();
+                this._searchService = null;
+            }
+        }
+
+        public Task<TRole> FindByIdAsync(string roleID,
+                CancellationToken cancellationToken) {
+            _ = roleID ?? throw new ArgumentNullException(nameof(roleID));
             throw new NotImplementedException();
         }
 
-        public Task<TRole> FindByIdAsync(string roleId, CancellationToken cancellationToken) {
+        public Task<TRole> FindByNameAsync(string normalisedRoleName,
+                CancellationToken cancellationToken) {
+            _ = normalisedRoleName
+                ?? throw new ArgumentNullException(nameof(normalisedRoleName));
             throw new NotImplementedException();
         }
 
-        public Task<TRole> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken) {
+        public Task<IList<Claim>> GetClaimsAsync(TRole role,
+                CancellationToken cancellationToken = default) {
+            _ = role ?? throw new ArgumentNullException(nameof(role));
             throw new NotImplementedException();
         }
 
-        public Task<IList<Claim>> GetClaimsAsync(TRole role, CancellationToken cancellationToken = default) {
+        public Task<string> GetNormalizedRoleNameAsync(TRole role,
+                CancellationToken cancellationToken) {
+            _ = role ?? throw new ArgumentNullException(nameof(role));
             throw new NotImplementedException();
         }
 
-        public Task<string> GetNormalizedRoleNameAsync(TRole role, CancellationToken cancellationToken) {
+        public Task<string> GetRoleIdAsync(TRole role,
+                CancellationToken cancellationToken) {
+            _ = role ?? throw new ArgumentNullException(nameof(role));
             throw new NotImplementedException();
         }
 
-        public Task<string> GetRoleIdAsync(TRole role, CancellationToken cancellationToken) {
+        public Task<string> GetRoleNameAsync(TRole role,
+                CancellationToken cancellationToken) {
+            _ = role ?? throw new ArgumentNullException(nameof(role));
             throw new NotImplementedException();
         }
 
-        public Task<string> GetRoleNameAsync(TRole role, CancellationToken cancellationToken) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="claim"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task RemoveClaimAsync(TRole role,
+                Claim claim,
+                CancellationToken cancellationToken = default) {
             throw new NotImplementedException();
         }
 
-        public Task RemoveClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="normalizedName"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task SetNormalizedRoleNameAsync(TRole role,
+                string normalizedName,
+                CancellationToken cancellationToken) {
             throw new NotImplementedException();
         }
 
-        public Task SetNormalizedRoleNameAsync(TRole role, string normalizedName, CancellationToken cancellationToken) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="roleName"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task SetRoleNameAsync(TRole role,
+                string roleName,
+                CancellationToken cancellationToken) {
             throw new NotImplementedException();
         }
 
-        public Task SetRoleNameAsync(TRole role, string roleName, CancellationToken cancellationToken) {
+        /// <summary>
+        /// This method is not implemented, because the current implementation
+        /// does not allow for modifying the user database.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="cancellationToken"></param>
+        /// <exception cref="NotImplementedException">Unconditionally.
+        /// </exception>
+        public Task<IdentityResult> UpdateAsync(TRole role,
+                CancellationToken cancellationToken) {
             throw new NotImplementedException();
         }
 
-        public Task<IdentityResult> UpdateAsync(TRole role, CancellationToken cancellationToken) {
-            throw new NotImplementedException();
-        }
+        #region Private fields
+        private ILdapSearchService _searchService;
+        #endregion
     }
 }
